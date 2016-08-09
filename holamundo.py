0397e0973659dc2683c971ab87375af48c20b7a2
fausto andrade <ayf1207@gmail.com>
2016-08-08 20:35:18 -0500

    actualizacion

---
+++ holamundo.py
@@ -0,0 +1,106 @@
+
+# coding: utf-8
+
+# In[1]:
+
+print 'hola mundo'
+
+
+# In[1]:
+
+2+4*6/8.0-3
+
+
+# In[2]:
+
+3/2
+
+
+# In[3]:
+
+3.0/2.0
+
+
+# In[4]:
+
+3+5j
+
+
+# In[5]:
+
+(3+5j)-(1-1j)
+
+
+# In[6]:
+
+abs(3+5j)
+
+
+# In[7]:
+
+5**3
+
+
+# In[9]:
+
+int(_3)
+
+
+# In[11]:
+
+float(_1)
+
+
+# In[14]:
+
+round(_3)
+
+
+# In[15]:
+
+max(8,10,20,30)
+
+
+# In[16]:
+
+num=10
+
+
+# In[17]:
+
+print num
+
+
+# In[18]:
+
+x,y,z,a=1,2,3,4
+
+
+# In[20]:
+
+print x
+print y
+print z
+print a
+
+
+# In[22]:
+
+x,y,z,a
+
+
+# In[21]:
+
+x,y,z,a=a,z,y,x
+print x,y,z,a
+
+
+# In[28]:
+
+x=5 
+y=8
+if x>y: 
+    print ("el mayor es",x)
+else: 
+    print ('el menor es',y)
+
